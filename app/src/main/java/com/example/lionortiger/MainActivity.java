90340658d9207c52c2cc9a2eec8f529d7535f654
Mostafa2293 <mostafamohamed05@gmail.com>
2019-12-05 09:42:32 +0200

    adding animation method to mainActivity

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lionortiger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void imageViewIsTapped(View imageView){
+
+        ImageView tappedImageView = (ImageView) imageView;
+
+        tappedImageView.setTranslationX(-2000);
+        tappedImageView.setImageResource(R.drawable.tiger);
+
+        tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+
+
+
     }
 }
